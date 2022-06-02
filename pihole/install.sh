43f3ef9d99ddf01592374e4b5316c44f15f50d25
Tobias Svedberg <svedberg.tobias@gmail.com>
2022-06-02 16:39:13 +0200

    Added terrafrom for piehole

--- pihole/install.sh
+++ pihole/install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 cloudflare_url="https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb"
 pihole_url="https://install.pi-hole.net"
+pihole_pass="${1}"
 
-apt update && atp upgrade && apt install curl git 
+apt update && apt -y upgrade && apt -y install curl git 
 
 # Download Cloudflared
 curl -LsO "${cloudflare_url}"
@@ -10,11 +11,8 @@ curl -LsO "${cloudflare_url}"
 # Install Cloudflared
 apt install -y ./cloudflared-linux-amd64.deb
 
-# Starting Cloudflared proxy
-cloudflared proxy-dns --port 5053
-
-# Create a systemd server for Cloudflared
-sudo tee /etc/systemd/system/cloudflared-proxy-dns.service >/dev/null <<EOF
+# Configure Cloudflared proxy
+tee /etc/systemd/system/cloudflared-proxy-dns.service >/dev/null <<EOF
 [Unit]
 Description=DNS over HTTPS (DoH) proxy client
 Wants=network-online.target nss-lookup.target
@@ -24,7 +22,7 @@ Before=nss-lookup.target
 AmbientCapabilities=CAP_NET_BIND_SERVICE
 CapabilityBoundingSet=CAP_NET_BIND_SERVICE
 DynamicUser=yes
-ExecStart=/usr/local/bin/cloudflared proxy-dns
+ExecStart=/usr/local/bin/cloudflared proxy-dns --port 5053
 
 [Install]
 WantedBy=multi-user.target
@@ -33,13 +31,8 @@ EOF
 # Enable Cloudflared service
 systemctl enable --now cloudflared-proxy-dns
 
-# Change system DNS-server
-rm -f /etc/resolv.conf
-echo nameserver 127.0.0.1 | tee /etc/resolv.conf >/dev/null
-
-
 # Create password for pihole
-pihole_pass=$(echo -n supersecurepassword | sha256sum | awk '{printf "%s",$1 }')
+pihole_pass=$(echo -n "${pihole_pass}" | sha256sum -z | sed 's/\s.*$//'| sha256sum | sed 's/\s.*$//')
 
 # Create pihole config directory
 mkdir -p /etc/pihole
@@ -50,7 +43,7 @@ PIHOLE_INTERFACE=eth0
 IPV4_ADDRESS=10.0.0.53/24
 IPV6_ADDRESS=
 PIHOLE_DNS_1=127.0.0.1#5053
-PIHOLE_DNS_2=127.0.0.1#5053
+PIHOLE_DNS_2=1.1.1.1
 QUERY_LOGGING=true
 INSTALL_WEB_SERVER=true
 INSTALL_WEB_INTERFACE=true
@@ -64,5 +57,11 @@ BLOCKING_ENABLED=true
 EOF
 
 # Install pihole
-#curl -L https://install.pi-hole.net | bash /dev/stdin --unattended
-PIHOLE_SKIP_OS_CHECK=true bash /dev/stdin --unattended -c "$(curl -sSfL ${pihole_url})"  
+curl -L "${pihole_url}" | PIHOLE_SKIP_OS_CHECK=true bash /dev/stdin --unattended
+
+# Copy local DNS records
+mv /tmp/custom.list /etc/pihole/custom.list
+
+# Change system DNS-server
+rm -f /etc/resolv.conf
+echo nameserver 127.0.0.1 | tee /etc/resolv.conf >/dev/null
